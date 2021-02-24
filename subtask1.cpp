74e6a6fcf32460b29032718cd3afcf4c1fdf1d1f
SwarajGawande <cs1190406@iitd.ac.in>
2021-02-24 19:31:57 +0530

    some final additions

--- subtask1.cpp
+++ subtask1.cpp
@@ -15,18 +15,20 @@ void CallBackFunc(int event,int x,int y,int flags,void* userdata){
 }
 
 int main(int argc, char* argv[]){
+	if (argc!=2){printf("Please try again with Image file name as argument (example ./sub1 Image.jpg)\n");return -1;}
 	Mat cimage,image;
 	cimage= imread(argv[1]);
 	cvtColor(cimage, image, cv::COLOR_BGR2GRAY);
 	putText(image, "Select 4 points which are the corners of road of interest in correct order TL TR BR BL", Point(5,100), FONT_HERSHEY_DUPLEX, 1, Scalar(0,143,143), 2);
+	putText(image, "Press any key to carry on (not power key ofc)", Point(5,125), FONT_HERSHEY_DUPLEX, 1, Scalar(0,143,143), 2);
 	if (image.empty()){cout<<"error loading image"<<endl;return -1;}
 	namedWindow("Img",1);
 	setMouseCallback("Img",CallBackFunc,NULL);
 	imshow("Img",image);
 	waitKey(0);
-	if (inp.size()!=4){printf("You have not selected exactly 4 points");return -1;}
+	if (inp.size()!=4){printf("You have not selected exactly 4 points\n");return -1;}
 	if (!(inp[0].y<=inp[2].y&&inp[0].y<=inp[3].y&&inp[1].y<=inp[2].y&&inp[1].y<inp[3].y&&inp[0].x<inp[1].x&&inp[2].x>inp[3].x)){
-	printf("Please select the points in correct order TL TR BR BL");
+	printf("Please select the points in correct order TL TR BR BL\n");
 	return -1;}
 	set_2.push_back(Point2f(472, 52));
 	set_2.push_back(Point2f(800, 52));
@@ -42,9 +44,12 @@ int main(int argc, char* argv[]){
 	Mat croppedImg=im_out(crop_region);
 	imshow("crop", croppedImg);
 	waitKey(0);
-	bool check = imwrite("cropped.jpg", croppedImg);
-	if (!check) {printf("unable to save image");return -1;}
-	printf("Cropped image saved ");
+	bool check1 = imwrite("transformed.jpg", croppedImg);
+	if (!check1) {printf("unable to save transformed image\n");return -1;}
+	printf("Transformed image saved \n");
+	bool check2 = imwrite("cropped.jpg", croppedImg);
+	if (!check2) {printf("unable to save cropped image\n");return -1;}
+	printf("Cropped image saved \n");
 	
 	//return 0;	
 }
