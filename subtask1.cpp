5482662c2d09e6fc65d425305b58a1c77f096d1b
ishita <ishita.c22@gmail.com>
2021-02-24 11:28:13 +0530

    angle correction

--- subtask1.cpp
+++ subtask1.cpp
@@ -1,4 +1,5 @@
 #include <opencv2/highgui.hpp>
+#include "opencv2/opencv.hpp"
 #include <iostream>
 
 using namespace cv;
@@ -6,7 +7,7 @@ using namespace std;
 
 
 vector<Point2f> inp;
-
+vector<Point2f> set_2;
 void CallBackFunc(int event,int x,int y,int flags,void* userdata){
 	if(event==EVENT_LBUTTONDOWN){
 	inp.push_back(Point2f(x,y));
@@ -15,12 +16,25 @@ void CallBackFunc(int event,int x,int y,int flags,void* userdata){
 
 int main(int argc, char** argv){
 	Mat image;
-	image= imread("I1.jpg");
+	image= imread("empty.jpg");
 	if (image.empty()){cout<<"error loading image"<<endl;return -1;}
 	namedWindow("Img",1);
 	setMouseCallback("Img",CallBackFunc,NULL);
 	imshow("Img",image);
 	waitKey(0);
 	print(inp);
-	return 0;	
+	
+	set_2.push_back(Point2f(472, 52));
+	set_2.push_back(Point2f(800, 52));
+	set_2.push_back(Point2f(800, 830));
+	set_2.push_back(Point2f(472, 830));
+	
+	Mat hom=findHomography(inp, set_2);
+	Mat im_out;
+	warpPerspective(image, im_out, hom, image.size());
+	imshow("Img", image);
+	imshow("Output image", im_out);
+	waitKey(0);
+	
+	//return 0;	
 }
