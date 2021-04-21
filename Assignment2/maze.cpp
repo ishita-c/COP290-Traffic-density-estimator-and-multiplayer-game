2a02d3ccd29de7785af6b13b8488ac166496fcae
SwarajGawande <cs1190406@iitd.ac.in>
2021-04-21 12:42:28 +0530

    first commit

---
+++ Assignment2/maze.cpp
@@ -0,0 +1,175 @@
+//Purpy Pupple's amazing maze generator. 
+//Released under the CC-BY-SA 3.0 License and the GFDL
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+#include <time.h>
+#include "maze.h"
+#include "output.h"
+using namespace std;
+
+#define UP 0     //-y
+#define DOWN 1   //+y
+#define LEFT 2   //-x
+#define RIGHT 3  //+x
+
+#define backtrack//enable this to generate mazes using depth-first search. Don't enable both.
+
+
+long numin=1;     //Number of cells in the maze.
+const int xsize=40;
+const int ysize=40;
+
+cell MAZE[xsize][ysize];
+
+void initialize(){
+	//Initialize the maze!
+	for(int x=0;x<xsize;x++){
+		for(int y=0;y<ysize;y++){
+			//The maze cells on the edges of the maze are "in" to provide padding. Otherwise, all maze cells are not in.
+			MAZE[x][y].in   = (x==0||x==xsize-1||y==0||y==ysize-1)?1:0;
+			//All maze cells have all walls existing by default, except the perimeter cells.
+			MAZE[x][y].up   = (x==0||x==xsize-1||y==0)?0:1;
+			MAZE[x][y].left = (x==0||y==0||y==ysize-1)?0:1;
+		}
+	}
+	return;
+}
+void generate(){
+	int xcur=1, ycur=1;//start growing from the corner. It could theoretically start growing from anywhere, doesn't matter.
+	MAZE[xcur][ycur].in = 1;
+	int whichway;
+	bool success;
+	do{
+		while( MAZE[xcur][ycur-1].in&&MAZE[xcur][ycur+1].in&&
+			   MAZE[xcur-1][ycur].in&&MAZE[xcur+1][ycur].in ){
+				   //If all the neighbourhood cells are in, backtrack.
+				int xcur2=MAZE[xcur][ycur].prevx;
+				ycur=MAZE[xcur][ycur].prevy;
+				xcur=xcur2;
+		}
+		do{
+			//Randomly grow the maze if possible.
+			success=0;
+			whichway=rand()%4;
+			switch(whichway){
+			case UP:
+				if(!MAZE[xcur][ycur-1].in){
+					success=1;
+					MAZE[xcur][ycur].up=0;
+					MAZE[xcur][ycur-1].prevx=xcur;
+					MAZE[xcur][ycur-1].prevy=ycur;
+					ycur--;
+				}
+				break;
+			case DOWN:
+				if(!MAZE[xcur][ycur+1].in){
+					success=1;
+					MAZE[xcur][ycur+1].up=0;
+					MAZE[xcur][ycur+1].prevx=xcur;
+					MAZE[xcur][ycur+1].prevy=ycur;
+					ycur++;
+				}
+				break;
+			case LEFT:
+				if(!MAZE[xcur-1][ycur].in){
+					success=1;
+					MAZE[xcur][ycur].left=0;
+					MAZE[xcur-1][ycur].prevx=xcur;
+					MAZE[xcur-1][ycur].prevy=ycur;
+					xcur--;
+				}
+				break;
+			case RIGHT:
+				if(!MAZE[xcur+1][ycur].in){
+					success=1;
+					MAZE[xcur+1][ycur].left=0;
+					MAZE[xcur+1][ycur].prevx=xcur;
+					MAZE[xcur+1][ycur].prevy=ycur;
+					xcur++;
+				}
+				break;
+			}
+		}while(!success);
+		MAZE[xcur][ycur].in=1;
+		numin++; //Every iteration of this loop, one maze cell is added to the maze.
+	}while(numin<(xsize-2)*(ysize-2));
+	return;
+}
+string savebmp(int xspecial, int yspecial){
+	//save a bitmap file! the xspecial, yspecial pixel is coloured red.
+	FILE * outfile;
+	int extrabytes, paddedsize;
+	int x, y, n;
+	int width=(xsize-1)*2-1;
+	int height=(ysize-1)*2-1;
+
+	extrabytes = (4 - ((width * 3) % 4))%4; 
+
+	char filename[200];
+	
+	sprintf(filename, "%s_%dx%d_n%d.bmp", OUTFILE, xsize, ysize, numin);
+	paddedsize = ((width * 3) + extrabytes) * height;
+
+	unsigned int headers[13] = {paddedsize + 54, 0, 54, 40, width, height, 0, 0, paddedsize, 0, 0, 0, 0};
+
+	outfile = fopen(filename, "wb");
+	fprintf(outfile, "BM");
+
+	for (n = 0; n <= 5; n++){
+	   fprintf(outfile, "%c", headers[n] & 0x000000FF);
+	   fprintf(outfile, "%c", (headers[n] & 0x0000FF00) >> 8);
+	   fprintf(outfile, "%c", (headers[n] & 0x00FF0000) >> 16);
+	   fprintf(outfile, "%c", (headers[n] & (unsigned int) 0xFF000000) >> 24);
+	}
+
+	fprintf(outfile, "%c", 1);fprintf(outfile, "%c", 0);
+	fprintf(outfile, "%c", 24);fprintf(outfile, "%c", 0);
+
+	for (n = 7; n <= 12; n++){
+	   fprintf(outfile, "%c", headers[n] & 0x000000FF);
+	   fprintf(outfile, "%c", (headers[n] & 0x0000FF00) >> 8);
+	   fprintf(outfile, "%c", (headers[n] & 0x00FF0000) >> 16);
+	   fprintf(outfile, "%c", (headers[n] & (unsigned int) 0xFF000000) >> 24);
+	}
+
+	//Actual writing of data begins here:
+	for(y = 0; y <= height - 1; y++){
+		for(x = 0; x <= width - 1; x++){
+			if(x%2 == 1 && y%2 == 1){
+				if(x/2+1 == xspecial && y/2+1 == yspecial) RED;
+				else{
+					if(MAZE[x/2+1][y/2+1].in) WHITE; else BLACK;
+				}
+			}else if(x%2 == 0 && y%2 == 0){
+				BLACK;
+			}else if(x%2 == 0 && y%2 == 1){
+				if(MAZE[x/2+1][y/2+1].left) BLACK; else WHITE;
+			}else if(x%2 == 1 && y%2 == 0){
+				if(MAZE[x/2+1][y/2+1].up) BLACK; else WHITE;
+			}
+		}
+		if (extrabytes){     // See above - BMP lines must be of lengths divisible by 4.
+			for (n = 1; n <= extrabytes; n++){
+				fprintf(outfile, "%c", 0);
+			}
+		}
+	}
+	printf("file printed: %s\n", filename); 
+	fclose(outfile);
+	string s=filename;
+	return s;
+}
+
+
+string maze(){
+	srand((unsigned int)time(NULL)); //seed random number generator with system time
+	initialize();      //initialize the maze
+	generate();
+	return savebmp(0,0);
+}
+
+
+
+
+
